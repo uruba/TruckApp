95056ee53d6d06ecb2f844533c57da7904c8453d
Václav Uruba <uruba@outlook.com>
2015-11-07 21:19:59 +0100

    – added SearchView into MeetupListFragment

--- app/src/main/java/cz/uruba/ets2mpcompanion/fragments/MeetupListFragment.java
+++ app/src/main/java/cz/uruba/ets2mpcompanion/fragments/MeetupListFragment.java
@@ -3,9 +3,14 @@ package cz.uruba.ets2mpcompanion.fragments;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -24,13 +29,15 @@ import cz.uruba.ets2mpcompanion.interfaces.JsoupDataReceiver;
 import cz.uruba.ets2mpcompanion.model.MeetupInfo;
 import cz.uruba.ets2mpcompanion.tasks.FetchJsoupDataTask;
 
-public class MeetupListFragment extends Fragment implements JsoupDataReceiver {
+public class MeetupListFragment extends Fragment implements JsoupDataReceiver, SearchView.OnQueryTextListener {
     @Bind(R.id.recyclerview_meetuplist) RecyclerView meetupList;
     @Bind(R.id.fab) FloatingActionButton fab;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_meetuplist, container, false);
+
+        setHasOptionsMenu(true);
 
         ButterKnife.bind(this, view);
 
@@ -44,6 +51,15 @@ public class MeetupListFragment extends Fragment implements JsoupDataReceiver {
         fetchMeetupList();
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_meetup_search, menu);
+
+        MenuItem item = menu.findItem(R.id.action_meetup_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+        searchView.setOnQueryTextListener(this);
     }
 
     private void fetchMeetupList() {
@@ -103,5 +119,15 @@ public class MeetupListFragment extends Fragment implements JsoupDataReceiver {
     @Override
     public void handleIOException(IOException e) {
 
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
