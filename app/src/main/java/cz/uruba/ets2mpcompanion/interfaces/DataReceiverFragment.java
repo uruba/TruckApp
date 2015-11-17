83dac20b8aa743c6e2f8818532d5748606f05c20
Václav Uruba <uruba@outlook.com>
2015-11-17 15:12:17 +0100

    – animated hiding/showing of the floating action button when being tapped on

--- app/src/main/java/cz/uruba/ets2mpcompanion/interfaces/DataReceiverFragment.java
+++ app/src/main/java/cz/uruba/ets2mpcompanion/interfaces/DataReceiverFragment.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 
 import java.util.Date;
@@ -15,9 +16,14 @@ import cz.uruba.ets2mpcompanion.R;
 
 public abstract class DataReceiverFragment<T> extends Fragment implements DataReceiver<T> {
     protected Date lastUpdated;
+    protected FABStateChangeListener fabStateChangeListener;
     @Bind(R.id.loading_overlay) protected FrameLayout loadingOverlay;
     @Bind(R.id.fab) protected FloatingActionButton fab;
     @Bind(R.id.fragment_wrapper) protected FrameLayout fragmentWrapper;
+
+    public DataReceiverFragment() {
+        fabStateChangeListener = new FABStateChangeListener();
+    }
 
     public Date getLastUpdated() {
         return lastUpdated;
@@ -28,6 +34,7 @@ public abstract class DataReceiverFragment<T> extends Fragment implements DataRe
         fadeInAnimation.setDuration(200);
         loadingOverlay.setAnimation(fadeInAnimation);
         loadingOverlay.setVisibility(View.VISIBLE);
+        fab.hide(fabStateChangeListener.loadingOverlayShown());
     }
 
     protected void hideLoadingOverlay() {
@@ -35,6 +42,7 @@ public abstract class DataReceiverFragment<T> extends Fragment implements DataRe
         fadeOutAnimation.setDuration(200);
         loadingOverlay.setAnimation(fadeOutAnimation);
         loadingOverlay.setVisibility(View.GONE);
+        fab.show(fabStateChangeListener.loadingOverlayHidden());
     }
 
     protected void hideLoadingOverlayOnMainLooper() {
@@ -45,5 +53,33 @@ public abstract class DataReceiverFragment<T> extends Fragment implements DataRe
                 hideLoadingOverlay();
             }
         });
+    }
+
+    protected static class FABStateChangeListener extends FloatingActionButton.OnVisibilityChangedListener {
+        private boolean isLoadingOverlayShown = false;
+
+        @Override
+        public void onShown(FloatingActionButton fab) {
+            if(isLoadingOverlayShown) {
+                fab.hide();
+            }
+        }
+
+        @Override
+        public void onHidden(FloatingActionButton fab) {
+            if(!isLoadingOverlayShown) {
+                fab.show();
+            }
+        }
+
+        public FABStateChangeListener loadingOverlayShown() {
+            isLoadingOverlayShown = true;
+            return this;
+        }
+
+        public FABStateChangeListener loadingOverlayHidden() {
+            isLoadingOverlayShown = false;
+            return this;
+        }
     }
 }
