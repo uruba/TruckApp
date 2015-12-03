da7e7b9b0e915663af87a035c0a632761c78e2d0
Václav Uruba <uruba@outlook.com>
2015-12-03 23:49:34 +0100

    – added MeetupDetailActivity skeleton

--- app/src/main/java/cz/uruba/ets2mpcompanion/MeetupDetailActivity.java
+++ app/src/main/java/cz/uruba/ets2mpcompanion/MeetupDetailActivity.java
@@ -1,6 +1,23 @@
 package cz.uruba.ets2mpcompanion;
 
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import cz.uruba.ets2mpcompanion.interfaces.ThemedActivity;
 
 public class MeetupDetailActivity extends ThemedActivity {
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+    }
 }
