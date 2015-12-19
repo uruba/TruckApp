1792da21657193fce779d7d61054282235b81d1e
Václav Uruba <uruba@outlook.com>
2015-12-19 23:03:26 +0100

    – added GAME_TIME URL constant

--- app/src/main/java/cz/uruba/ets2mpcompanion/constants/URL.java
+++ app/src/main/java/cz/uruba/ets2mpcompanion/constants/URL.java
@@ -2,5 +2,6 @@ package cz.uruba.ets2mpcompanion.constants;
 
 public class URL {
     public static final String SERVER_LIST = "http://api.ets2mp.com/servers/";
+    public static final String GAME_TIME = "http://api.ets2mp.com/game_time/";
     public static final String MEETUP_LIST = "http://ets2c.com/";
 }
