5b75d3d2ebbaabb42f585611c435828a7527d56b
Václav Uruba <uruba@outlook.com>
2015-12-30 23:43:09 +0100

    – automatic update for the ServerTimeTextView

--- app/src/main/java/cz/uruba/ets2mpcompanion/views/ServerTimeTextView.java
+++ app/src/main/java/cz/uruba/ets2mpcompanion/views/ServerTimeTextView.java
@@ -1,45 +1,65 @@
 package cz.uruba.ets2mpcompanion.views;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.AttributeSet;
-import android.widget.TextView;
 
 import java.text.DateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
 import cz.uruba.ets2mpcompanion.R;
+import cz.uruba.ets2mpcompanion.interfaces.AutoUpdatedTextView;
 import cz.uruba.ets2mpcompanion.model.ServerTime;
 
-public class ServerTimeTextView extends TextView {
+public class ServerTimeTextView extends AutoUpdatedTextView {
+    // hard-coded interval of 10 seconds = 10000 ms
+    private static final long UPDATE_INTERVAL = 10000;
+
     private ServerTime serverTime;
     private Context context;
 
+    private DateFormat dateFormat = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault());
+
     public ServerTimeTextView(Context context) {
-        super(context);
-        setContext(context);
+        this(context, null);
     }
 
     public ServerTimeTextView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        setContext(context);
+        this(context, attrs, Resources.getSystem().getIdentifier("textViewStyle", "attr", "android"));
     }
 
     public ServerTimeTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setContext(context);
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+    }
+
+    @Override
+    public boolean startAutoRefresh() {
+        return serverTime != null && super.startAutoRefresh();
+    }
+
+    @Override
+    protected long processAndDisplay() {
+        Date serverTimeAtRefresh = serverTime.getServerTime().first;
+        Date realTimeAtRefresh = serverTime.getServerTime().second;
+
+        long computedServerTime = serverTimeAtRefresh.getTime() + ((System.currentTimeMillis() - realTimeAtRefresh.getTime()) / (10 * 1000)) * 60 * 1000;
+
+        setText(String.format(
+                context.getString(R.string.server_time),
+                dateFormat.format(new Date(computedServerTime))
+                )
+        );
+
+        return UPDATE_INTERVAL;
     }
 
     public void setServerTime(ServerTime serverTime) {
         this.serverTime = serverTime;
-
-        DateFormat dateFormat = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault());
-
-        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-
-        setText(String.format(
-                context.getString(R.string.server_time),
-                dateFormat.format(serverTime.getServerTime().first)));
+        restartAutoRefresh();
     }
 
     private void setContext(Context context) {
