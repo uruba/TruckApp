6bc860ae9a3c67839c1e5a274ee45b54454693fd
Václav Uruba <uruba@outlook.com>
2016-01-08 20:59:03 +0100

    – test all the overflow menu entries

--- app/src/androidTest/java/cz/uruba/ets2mpcompanion/test/MainActivityTest.java
+++ app/src/androidTest/java/cz/uruba/ets2mpcompanion/test/MainActivityTest.java
@@ -4,17 +4,21 @@ import android.support.test.annotation.UiThreadTest;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.v4.app.Fragment;
 import android.test.ActivityInstrumentationTestCase2;
-import android.view.KeyEvent;
 
 import org.junit.Before;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import cz.uruba.ets2mpcompanion.MainActivity;
+import cz.uruba.ets2mpcompanion.*;
+import cz.uruba.ets2mpcompanion.R;
 import cz.uruba.ets2mpcompanion.fragments.MeetupListFragment;
 import cz.uruba.ets2mpcompanion.fragments.ServerListFragment;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static cz.uruba.ets2mpcompanion.test.matchers.WithToolbarTitle.withToolbarTitle;
@@ -22,6 +26,15 @@ import static org.hamcrest.Matchers.is;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
     private MainActivity activity;
+
+    private final List<Integer> optionsMenuEntries = new ArrayList<>(
+            Arrays.asList(
+                    R.string.action_settings,
+                    R.string.action_ets2mp_home,
+                    R.string.action_ets2mp_forum,
+                    R.string.action_ets2mp_convoys
+            )
+    );
 
     public MainActivityTest() {
         super(MainActivity.class);
@@ -52,9 +65,13 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     }
 
     public void testOptionsMenu() {
-        getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
+        for (int optionsMenuEntry : optionsMenuEntries) {
+            openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
 
-        onView(ViewMatchers.withText(cz.uruba.ets2mpcompanion.R.string.action_settings)).perform(click());
-        onView(ViewMatchers.withId(cz.uruba.ets2mpcompanion.R.id.toolbar)).check(matches(withToolbarTitle(is(activity.getString(cz.uruba.ets2mpcompanion.R.string.action_settings)))));
+            onView(ViewMatchers.withText(optionsMenuEntry)).perform(click());
+            onView(ViewMatchers.withId(cz.uruba.ets2mpcompanion.R.id.toolbar)).check(matches(withToolbarTitle(is(activity.getString(optionsMenuEntry)))));
+
+            pressBack();
+        }
     }
 }
