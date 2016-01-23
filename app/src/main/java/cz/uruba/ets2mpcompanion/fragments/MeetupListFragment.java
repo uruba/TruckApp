d825cf53ac74b40b0331ffcac4d6ddc1c1d7c43d
Václav Uruba <uruba@outlook.com>
2016-01-23 14:45:44 +0100

    – first implementation of filtering meetups by a server name

--- app/src/main/java/cz/uruba/ets2mpcompanion/fragments/MeetupListFragment.java
+++ app/src/main/java/cz/uruba/ets2mpcompanion/fragments/MeetupListFragment.java
@@ -1,8 +1,12 @@
 package cz.uruba.ets2mpcompanion.fragments;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
@@ -12,6 +16,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -32,12 +37,21 @@ public class MeetupListFragment extends DataReceiverFragment<ArrayList<MeetupInf
 
     private List<MeetupInfo> meetups = new ArrayList<>();
     private List<MeetupInfo> filteredMeetups = new ArrayList<>();
+    public static final int MEETUP_FIELD_LOCATION = 1;
+    public static final int MEETUP_FIELD_ORGANISER = 1 << 1;
+    public static final int MEETUP_FIELD_LANGUAGE = 1 << 2;
+    private CharSequence[] serverLiterals;
 
     private List<MenuItem> menuItems = new ArrayList<>();
+
+    private SharedPreferences sharedPref;
+    public static final String PREF_SERVER_FILTER_SETTING = "preference_server_filter_setting";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_meetuplist, container, false);
+
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(getContext());
 
         setHasOptionsMenu(true);
 
@@ -54,6 +68,14 @@ public class MeetupListFragment extends DataReceiverFragment<ArrayList<MeetupInf
 
         listAdapter = new MeetupListAdapter(getContext(), new ArrayList<MeetupInfo>(), this);
         meetupList.setAdapter(listAdapter);
+
+        serverLiterals = new CharSequence[] {
+            getString(R.string.server_name_all),
+            getString(R.string.server_name_eu1),
+            getString(R.string.server_name_eu2),
+            getString(R.string.server_name_eu3),
+            getString(R.string.server_name_us1)
+        };
 
         fetchMeetupList();
 
@@ -96,7 +118,7 @@ public class MeetupListFragment extends DataReceiverFragment<ArrayList<MeetupInf
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_meetup_filter:
-
+                    showFilterDialog();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -117,6 +139,7 @@ public class MeetupListFragment extends DataReceiverFragment<ArrayList<MeetupInf
     public void resetMeetupList() {
         if(listAdapter != null) {
             listAdapter.refreshAdapter(meetups);
+            filterByServer();
         }
     }
 
@@ -127,6 +150,8 @@ public class MeetupListFragment extends DataReceiverFragment<ArrayList<MeetupInf
         lastUpdated = new Date();
 
         listAdapter.setDataCollection(new ArrayList<>(meetups));
+
+        filterByServer();
 
         hideLoadingOverlay();
 
@@ -161,19 +186,50 @@ public class MeetupListFragment extends DataReceiverFragment<ArrayList<MeetupInf
     
     @Override
     public boolean onQueryTextChange(String newText) {
+        filterByText(newText, MEETUP_FIELD_LOCATION | MEETUP_FIELD_ORGANISER | MEETUP_FIELD_LANGUAGE);
+
+        return true;
+    }
+
+    private void showFilterDialog() {
+        new AlertDialog.Builder(getContext())
+                .setTitle(getString(R.string.filter_servers))
+                .setSingleChoiceItems(
+                        serverLiterals,
+                        sharedPref.getInt(PREF_SERVER_FILTER_SETTING, 0),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                sharedPref.edit().putInt(PREF_SERVER_FILTER_SETTING, which).apply();
+                                filterByServer();
+                                dialog.dismiss();
+                                Toast.makeText(getContext(), serverLiterals[which], Toast.LENGTH_LONG).show();
+                            }
+                })
+                .create()
+                .show();
+    }
+
+    private void filterByText(String newText, int fieldsFlag) {
         if (meetups.size() < 1) {
-            return true;
+            return;
         }
 
         newText = newText.toLowerCase();
 
         filteredMeetups = new ArrayList<>();
         for (MeetupInfo meetup : meetups) {
-            String [] fields = new String[3];
 
-            fields[0] = meetup.getLocation().toLowerCase();
-            fields[1] = meetup.getOrganiser().toLowerCase();
-            fields[2] = meetup.getLanguage().toLowerCase();
+            List<String> fields = new ArrayList<>();
+            if ((fieldsFlag & MEETUP_FIELD_LOCATION) == MEETUP_FIELD_LOCATION) {
+                fields.add(meetup.getLocation().toLowerCase());
+            }
+            if ((fieldsFlag & MEETUP_FIELD_ORGANISER) == MEETUP_FIELD_ORGANISER) {
+                fields.add(meetup.getOrganiser().toLowerCase());
+            }
+            if ((fieldsFlag & MEETUP_FIELD_LANGUAGE) == MEETUP_FIELD_LANGUAGE) {
+                fields.add(meetup.getLanguage().toLowerCase());
+            }
 
             for (String field : fields) {
                 if (field.contains(newText)) {
@@ -185,8 +241,11 @@ public class MeetupListFragment extends DataReceiverFragment<ArrayList<MeetupInf
 
         listAdapter.refreshAdapter(filteredMeetups);
         meetupList.scrollToPosition(0);
+    }
 
-        return true;
+    private void filterByServer() {
+        int which = sharedPref.getInt(PREF_SERVER_FILTER_SETTING, 0);
+        filterByText(which == 0 ? "" : serverLiterals[which].toString(), MEETUP_FIELD_LOCATION);
     }
 
     private void showMenuItems() {
