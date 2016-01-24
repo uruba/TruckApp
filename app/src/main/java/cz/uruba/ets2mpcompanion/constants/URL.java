3ec2b36affcedc587992a51e18b31afc8286118b
Václav Uruba <uruba@outlook.com>
2016-01-24 18:36:30 +0100

    – https version of the main ETS2MP API endpoints

--- app/src/main/java/cz/uruba/ets2mpcompanion/constants/URL.java
+++ app/src/main/java/cz/uruba/ets2mpcompanion/constants/URL.java
@@ -1,8 +1,8 @@
 package cz.uruba.ets2mpcompanion.constants;
 
 public class URL {
-    public static final String SERVER_LIST = "http://api.ets2mp.com/servers/";
-    public static final String GAME_TIME = "http://api.ets2mp.com/game_time/";
+    public static final String SERVER_LIST = "https://api.ets2mp.com/servers/";
+    public static final String GAME_TIME = "https://api.ets2mp.com/game_time/";
     public static final String MEETUP_LIST = "http://ets2c.com/";
 
     public static final String ETS2MP_HOMEPAGE = "http://ets2mp.com";
