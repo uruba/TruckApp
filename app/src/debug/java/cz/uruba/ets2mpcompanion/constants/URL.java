75ce423d613f69b4195cdfbeb2e17013a42174c5
Václav Uruba <uruba@outlook.com>
2016-02-03 23:04:03 +0100

    – mock the web server for the "debug" build configuration

---
+++ app/src/debug/java/cz/uruba/ets2mpcompanion/constants/URL.java
@@ -0,0 +1,11 @@
+package cz.uruba.ets2mpcompanion.constants;
+
+public class URL {
+    public static final String SERVER_LIST = "http://localhost:52734/servers/";
+    public static final String GAME_TIME = "http://localhost:52734/game_time/";
+    public static final String MEETUP_LIST = "http://localhost:52734/meetups/";
+
+    public static final String ETS2MP_HOMEPAGE = "http://localhost:52734/";
+    public static final String ETS2MP_FORUM = "http://localhost:52734/";
+    public static final String ETS2MP_CONVOYS = MEETUP_LIST;
+}
