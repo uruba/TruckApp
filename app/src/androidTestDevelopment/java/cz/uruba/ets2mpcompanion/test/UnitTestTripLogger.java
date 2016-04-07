554d4b9fcc68021f84d50ec1a698f4e09c3e9d6d
Václav Uruba <uruba@outlook.com>
2016-04-07 19:11:58 +0200

    – early draft version of TripLoggerContract class with a corresponding unit test class

---
+++ app/src/androidTestDevelopment/java/cz/uruba/ets2mpcompanion/test/UnitTestTripLogger.java
@@ -0,0 +1,43 @@
+package cz.uruba.ets2mpcompanion.test;
+
+import android.test.InstrumentationTestCase;
+
+import cz.uruba.ets2mpcompanion.model.DB.Contracts.TripLoggerContract;
+
+public class UnitTestTripLogger extends InstrumentationTestCase {
+
+    public void testBaseUri() {
+          assertEquals(
+                  "content://cz.uruba.ets2mpcompanion.triplogger",
+                  TripLoggerContract.BASE_CONTENT_URI.toString()
+          );
+    }
+
+    public void testTripLoggerEntryUri() {
+        assertEquals(
+                "content://cz.uruba.ets2mpcompanion.triplogger/entry",
+                TripLoggerContract.TripLoggerEntry.CONTENT_URI.toString()
+        );
+    }
+
+    public void testCargoDefinitionUri() {
+        assertEquals(
+                "content://cz.uruba.ets2mpcompanion.triplogger/cargo",
+                TripLoggerContract.CargoDefinition.CONTENT_URI.toString()
+        );
+    }
+
+    public void testCityUri() {
+        assertEquals(
+                "content://cz.uruba.ets2mpcompanion.triplogger/city",
+                TripLoggerContract.City.CONTENT_URI.toString()
+        );
+    }
+
+    public void testGameUri() {
+        assertEquals(
+                "content://cz.uruba.ets2mpcompanion.triplogger/game",
+                TripLoggerContract.Game.CONTENT_URI.toString()
+        );
+    }
+}
---
+++ app/src/main/java/cz/uruba/ets2mpcompanion/model/DB/Contracts/TripLoggerContract.java
@@ -0,0 +1,59 @@
+package cz.uruba.ets2mpcompanion.model.DB.Contracts;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+import cz.uruba.ets2mpcompanion.BuildConfig;
+
+public final class TripLoggerContract {
+
+    public static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".triplogger";
+
+    public static final Uri BASE_CONTENT_URI = new Uri.Builder()
+            .scheme("content")
+            .authority(AUTHORITY)
+            .build();
+
+    public static final class TripLoggerEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI
+                .buildUpon()
+                .appendPath("entry")
+                .build();
+        public static final String TABLE_NAME = "tripLoggerEntries";
+        public static final String COLUMN_ORIGIN = "originCityId";
+        public static final String COLUMN_DESTINATION = "destinationCityId";
+        public static final String COLUMN_DISTANCE = "distance";
+        public static final String COLUMN_CARGO_ID = "cargoId";
+    }
+
+    public static final class CargoDefinition implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI
+                .buildUpon()
+                .appendPath("cargo")
+                .build();
+        public static final String TABLE_NAME = "cargoDefinitions";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_TONNAGE = "tonnage";
+    }
+
+    public static final class City implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI
+                .buildUpon()
+                .appendPath("city")
+                .build();
+        public static final String TABLE_NAME = "cities";
+        public static final String COLUMN_NAME = "name";
+    }
+
+    public static final class Game implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI
+                .buildUpon()
+                .appendPath("game")
+                .build();
+        public static final String TABLE_NAME = "games";
+        public static final String COLUMN_NAME = "name";
+    }
+
+    private TripLoggerContract() {
+
+    }
+}
