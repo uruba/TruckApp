0d9392cfa687fc5a0908be3743288b0da1241bbd
Václav Uruba <uruba@outlook.com>
2016-05-15 11:48:32 +0200

    – new URL for the meetup API

--- app/src/production/java/cz/uruba/ets2mpcompanion/constants/URL.java
+++ app/src/production/java/cz/uruba/ets2mpcompanion/constants/URL.java
@@ -3,7 +3,7 @@ package cz.uruba.ets2mpcompanion.constants;
 public class URL {
     public static final String SERVER_LIST = "https://api.ets2mp.com/v2/servers";
     public static final String GAME_TIME = "https://api.ets2mp.com/v2/game_time";
-    public static final String MEETUP_LIST = "http://api.truckapp.org/api/v1/meetup-list";
+    public static final String MEETUP_LIST = "http://api.truckapp.org/v1/meetup-list";
 
     public static final String ETS2MP_HOMEPAGE = "http://ets2mp.com/";
     public static final String ETS2MP_FORUM = "https://forum.truckersmp.com/";
