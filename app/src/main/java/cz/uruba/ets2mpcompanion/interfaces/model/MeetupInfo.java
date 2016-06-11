f39ba51644c39c943e05b40e00271699a6f3d59b
Václav Uruba <uruba@outlook.com>
2016-06-11 20:43:10 +0200

    Weaker access to some class fields

--- app/src/main/java/cz/uruba/ets2mpcompanion/interfaces/model/MeetupInfo.java
+++ app/src/main/java/cz/uruba/ets2mpcompanion/interfaces/model/MeetupInfo.java
@@ -3,16 +3,16 @@ package cz.uruba.ets2mpcompanion.interfaces.model;
 import java.io.Serializable;
 
 public abstract class MeetupInfo implements Serializable {
-    protected String server;
-    protected String when;
-    protected String location;
-    protected String organiser;
-    protected String language;
-    protected String participants;
-    protected String relativeURL;
-    protected String baseURL;
+    private String server;
+    private String when;
+    private String location;
+    private String organiser;
+    private String language;
+    private String participants;
+    private String relativeURL;
+    private String baseURL;
 
-    public MeetupInfo(String server, String when, String location, String organiser, String language, String participants, String relativeURL, String baseURL) {
+    protected MeetupInfo(String server, String when, String location, String organiser, String language, String participants, String relativeURL, String baseURL) {
         this.server = server;
         this.when = when;
         this.location = location;
