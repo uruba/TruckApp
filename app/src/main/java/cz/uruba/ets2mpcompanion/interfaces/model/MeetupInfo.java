e7f25e4ce35b41a36f78719689e7ecf10989f6ac
Václav Uruba <uruba@outlook.com>
2016-07-17 19:24:47 +0200

    – added a final modifier where applicable

--- app/src/main/java/cz/uruba/ets2mpcompanion/interfaces/model/MeetupInfo.java
+++ app/src/main/java/cz/uruba/ets2mpcompanion/interfaces/model/MeetupInfo.java
@@ -3,14 +3,14 @@ package cz.uruba.ets2mpcompanion.interfaces.model;
 import java.io.Serializable;
 
 public abstract class MeetupInfo implements Serializable {
-    private String server;
-    private String when;
-    private String location;
-    private String organiser;
-    private String language;
-    private String participants;
-    private String relativeURL;
-    private String baseURL;
+    private final String server;
+    private final String when;
+    private final String location;
+    private final String organiser;
+    private final String language;
+    private final String participants;
+    private final String relativeURL;
+    private final String baseURL;
 
     protected MeetupInfo(String server, String when, String location, String organiser, String language, String participants, String relativeURL, String baseURL) {
         this.server = server;
