542f25e9ce437d65ca5b6e9f866875c4516decfc
uruba <uruba@outlook.com>
2018-02-19 14:42:51 +0100

    New URLs for the server list and game time server

--- app/src/production/java/cz/uruba/ets2mpcompanion/constants/URL.java
+++ app/src/production/java/cz/uruba/ets2mpcompanion/constants/URL.java
@@ -1,8 +1,8 @@
 package cz.uruba.ets2mpcompanion.constants;
 
 public class URL {
-    public static final String SERVER_LIST = "https://api.ets2mp.com/v2/servers";
-    public static final String GAME_TIME = "https://api.ets2mp.com/v2/game_time";
+    public static final String SERVER_LIST = "https://api.truckersmp.com/v2/servers";
+    public static final String GAME_TIME = "https://api.truckersmp.com/v2/game_time";
     public static final String MEETUP_LIST = "http://api.truckapp.org/v1/meetup-list";
 
     public static final String ETS2MP_HOMEPAGE = "http://ets2mp.com/";
