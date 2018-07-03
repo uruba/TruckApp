b4f1a6ec71efb24e1c948789dcbb6dbf6a94487c
uruba <uruba@outlook.com>
2018-07-03 14:12:52 +0200

    Updated the ets2c.com URL to https prefix (should fix the event login process)

--- app/src/production/java/cz/uruba/ets2mpcompanion/constants/URL.java
+++ app/src/production/java/cz/uruba/ets2mpcompanion/constants/URL.java
@@ -5,8 +5,8 @@ public class URL {
     public static final String GAME_TIME = "https://api.truckersmp.com/v2/game_time";
     public static final String MEETUP_LIST = "http://api.truckapp.org/v1/meetup-list";
 
-    public static final String ETS2MP_HOMEPAGE = "http://ets2mp.com/";
+    public static final String ETS2MP_HOMEPAGE = "https://truckersmp.com/";
     public static final String ETS2MP_FORUM = "https://forum.truckersmp.com/";
-    public static final String ETS2MP_CONVOYS = "http://ets2c.com/";
-    public static final String TRUCKERS_EVENTS = "http://truckers.events/";
+    public static final String ETS2MP_CONVOYS = "https://ets2c.com/";
+    public static final String TRUCKERS_EVENTS = "https://truckers.events/";
 }
