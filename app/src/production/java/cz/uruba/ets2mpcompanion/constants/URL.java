1b6bb64c82d2c47fd9ba8815c4aab556848bd552
uruba <uruba@outlook.com>
2018-08-22 08:25:05 +0200

    Updated the API URL protocol http -> https

--- app/src/production/java/cz/uruba/ets2mpcompanion/constants/URL.java
+++ app/src/production/java/cz/uruba/ets2mpcompanion/constants/URL.java
@@ -3,7 +3,7 @@ package cz.uruba.ets2mpcompanion.constants;
 public class URL {
     public static final String SERVER_LIST = "https://api.truckersmp.com/v2/servers";
     public static final String GAME_TIME = "https://api.truckersmp.com/v2/game_time";
-    public static final String MEETUP_LIST = "http://api.truckapp.org/v1/meetup-list";
+    public static final String MEETUP_LIST = "https://api.truckapp.org/v1/meetup-list";
 
     public static final String ETS2MP_HOMEPAGE = "https://truckersmp.com/";
     public static final String ETS2MP_FORUM = "https://forum.truckersmp.com/";
